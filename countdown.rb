82d0c0f87b0b5f5776c89a8070b0aedeabfeb6bf
chic-state-0895 <brookeabryski@gmail.com>
2020-08-28 19:31:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,19 @@
 #write your code here
 
-def countdown(x)
-  x = 10
-  while x < 0
-    puts "{x} SECOND(S)!"
-    x -= 1
-    puts "HAPPY NEW YEAR!"
+def countdown(num)
+  while num > 0
+    puts "{num} SECOND(S)!"
+    num -= 1
 end
+
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  while num > 0 
+    puts "{num} SECOND(S)!"
+    sleep(1)
+      num -= 1
+end
+"HAPPY NEW YEAR!"
 end
